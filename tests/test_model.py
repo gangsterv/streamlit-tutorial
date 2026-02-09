cd17c9a167f657582fbec07f2b04c6c9482e72ec
Omar <49798376+gangsterv@users.noreply.github.com>
2026-02-09 03:55:32 +0200

    Update score calculation to use feature set X
    Fix prediction input by dropping 'Survived' column from data.

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,6 @@
 import joblib
 import pandas as pd
+from sklearn.metrics import accuracy_score
 
 # Load the model
 pipeline = joblib.load("models/model_joblib.pkl")
@@ -7,10 +8,13 @@ pipeline = joblib.load("models/model_joblib.pkl")
 # Create a pandas dataframe out of the list
 data = pd.read_csv("data/test_data.csv")
 
+X = data.drop(columns=["Survived"])
+y = data["Survived"]
+
 # Make a prediction
 # prediction = pipeline.predict(data)
 
-score = pipeline.score(data)
+score = pipeline.score(X)
 
 print("Score is:", score)
 
