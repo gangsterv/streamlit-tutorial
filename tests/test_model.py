0719096dde5663ccfcea5322a579a31e38490f78
Omar <49798376+gangsterv@users.noreply.github.com>
2026-02-09 03:40:18 +0200

    Remove age slider from test_model.py
    Removed age selection slider from the test model.

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,9 +4,6 @@ import pandas as pd
 # Load the model
 pipeline = joblib.load("models/model_joblib.pkl")
 
-age = st.select_slider("Age", options=list(range(0, 120)))
-
-
 # Create a pandas dataframe out of the list
 data = pd.read_csv("../data/test_data.csv")
 
